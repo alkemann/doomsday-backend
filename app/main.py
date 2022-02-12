274511f89df8179e9553b3f73b8608d5fc879712
Alexander Morland <alek.morland@gmail.com>
2022-02-12 11:10:26 +0100

    added db config and models

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
-from flask import Flask, jsonify, request, json
+from flask import Flask, jsonify, request
 from app.highscores import get_all_scores, get_score_by_id, save_score
+import os
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
-app.config["DEBUG"] = True
+app.config.from_object(os.environ["APP_SETTINGS"])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
 @app.route('/', methods=['GET'])
---
+++ app/models.py
@@ -0,0 +1,19 @@
+from main import db
+
+
+class Score(db.Model):
+    __tablename__ = 'scores'
+
+    id = db.Column(db.Integer, primary_key=True)
+    points = db.Column(db.Integer)
+    time = db.Column(db.Integer)
+    name = db.Column(db.String())
+
+    def __init__(self, points, time, name):
+        self.points = points
+        self.time = time
+        self.name = name
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
